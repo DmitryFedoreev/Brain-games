ea4dd5f95ca38f6924cc05fbfb331f4326aa7b97
DmitryFedoreev <fedoreev.doc@gmail.com>
2024-05-14 21:40:19 +0500

    Add file setup.cfg and brain_even.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,6 @@
 import prompt
 
 
-#greetings = 'brain-games\nWelcome to the Brain Games!'
-#print(greetings)
-
-
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
