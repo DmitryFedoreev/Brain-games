19df71f98829416a203d4d450c55543dcc5b72c8
DmitryFedoreev <fedoreev.doc@gmail.com>
2024-05-21 15:42:47 +0500

    Refactor common.py and README.md

--- brain_games/common.py
+++ brain_games/common.py
@@ -8,23 +8,34 @@ def hello():
     return name
 
 
+def play_round(generate_round):
+    question, correct_answer = generate_round()
+    print(f'Question: {question}')
+    user_answer = prompt.string('Your answer: ')
+    return user_answer, correct_answer
+
+
+def print_round_result(user_answer, correct_answer, name):
+    if user_answer == str(correct_answer):
+        print('Correct!')
+        return True
+    else:
+        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
+
+
 def play_game(game):
     name = hello()
     print(game['description'])
     count = 0
 
     while count < 3:
-        question, correct_answer = game['generate_round']()
-        print(f'Question: {question}')
-        user_answer = prompt.string('Your answer: ')
-
-        if user_answer == str(correct_answer):
-            print('Correct!')
+        user_answer, correct_answer = play_round(game['generate_round'])
+        if print_round_result(user_answer, correct_answer, name):
             count += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
             break
 
-        if count == 3:
-            print(f'Congratulations, {name}!')
+    if count == 3:
+        print(f'Congratulations, {name}!')
