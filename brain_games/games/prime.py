3676a8f8d0faeb6f5480773ea6046a365155c3fd
DmitryFedoreev <fedoreev.doc@gmail.com>
2024-05-21 17:17:02 +0500

    Refactor prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,10 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(n):
-    if n <= 1:
+    if n < 2:
         return False
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-
-    for i in range(5, int(n**0.5) + 1, 6):
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
